19ca66ab1eda2e23185d6f867408ee52db686803
Jerrold Koh <jerroldfisher@live.com>
2024-04-22 16:45:11 +0800

    Completed all producers and changed key to String
    Int serialisation for the key was not working.

--- images/sensor/SensorProducerApplication.java
+++ images/sensor/SensorProducerApplication.java
@@ -5,7 +5,7 @@ import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.apache.kafka.common.serialization.IntegerSerializer;
+//import org.apache.kafka.common.serialization.IntegerSerializer;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,29 +13,29 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Collection;
-import java.util.List;
+// import java.nio.file.Files;
+// import java.nio.file.Paths;
+// import java.util.Collection;
+// import java.util.List;
 import java.util.Properties;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.stream.Collectors;
+// import java.util.concurrent.ExecutionException;
+// import java.util.concurrent.Future;
+// import java.util.stream.Collectors;
 
 public class SensorProducerApplication {
 
-    private final Producer<Integer, String> producer;
+    private final Producer<String, String> producer;
     final String outTopic;
     // class constructor
-    public SensorProducerApplication(final Producer<Integer, String> producer, final String topic) {
+    public SensorProducerApplication(final Producer<String, String> producer, final String topic) {
         this.producer = producer;
         outTopic = topic;
     }
     // produce a record
     // Key: ID int, Value: JSON String
-    public void produce(int key, String value) {
+    public void produce(String key, String value) {
         try {
-            final ProducerRecord<Integer, String> producerRecord = new ProducerRecord<>(outTopic, Integer.valueOf(key), value);
+            final ProducerRecord<String, String> producerRecord = new ProducerRecord<>(outTopic, key, value);
             producer.send(producerRecord);
             System.out.println("Sent record to Broker: " + value);
             Thread.sleep(1000);
@@ -60,15 +60,15 @@ public class SensorProducerApplication {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Producer started!");
-        if (args.length < 2) {
+        if (args.length < 3) {
             throw new IllegalArgumentException(
-                    "This program takes two arguments: the path to an environment configuration file and" +
-                            "the path to the file with records to send" );
+                    "This program takes 3 arguments: the path to an environment configuration file," +
+                            "the path to the file with records to send, and the topic name" );
         }
 
         final Properties props = SensorProducerApplication.loadProperties(args[0]);
-        final String topic = "temperature-topic";
-        final Producer<Integer, String> producer = new KafkaProducer<>(props);
+        final String topic = args[2];//"temperature-topic";
+        final Producer<String, String> producer = new KafkaProducer<>(props);
         final SensorProducerApplication producerApp = new SensorProducerApplication(producer, topic);
 
         String filePath = args[1];
@@ -76,7 +76,7 @@ public class SensorProducerApplication {
             ObjectMapper mapper = new ObjectMapper();
             JsonNode jsonArray = mapper.readTree(new File(args[1]));
             for (JsonNode jsonNode : jsonArray){
-                int key = jsonNode.get("id").asInt();
+                String key = jsonNode.get("id").toString();
                 String value = jsonNode.toString();
                 producerApp.produce(key, value);
             }
