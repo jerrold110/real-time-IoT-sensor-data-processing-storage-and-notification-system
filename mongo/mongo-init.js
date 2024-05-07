697246dc222eb2c98aa0dc7724539193ba0cf51b
Jerrold Koh <jerroldfisher@live.com>
2024-05-07 15:39:14 +0800

    Mongo initialisation

--- mongo/mongo-init.js
+++ mongo/mongo-init.js
@@ -1,12 +1,20 @@
-db.createUser(
-    {
-        user: "abc",
-        pwd: "abc",
-        roles: [
-            {
-                role: "readWrite",
-                db: "a_db"
-            }
-        ]
-    }
-);
+// this creates the new user in the database sensors instead of test when i use db
+var adminDB = db.getSiblingDB('admin');
+adminDB.auth('root', 'root');
+
+adminDB = adminDB.getSiblingDB('sensors');
+adminDB.createUser({
+  user: 'sensor_user',
+  pwd: 'sensor_pass',
+  roles: 
+    [{
+        role: "readWrite",
+        db: "sensors"
+    }],
+});
+
+adminDB.createCollection('temperature');
+adminDB.createCollection('humidity');
+
+// mongosh -u root -p root
+// mongosh -u sensor_user -p sensor_pass --authenticationDatabase sensors
